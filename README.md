d734bcd9aec73d26928ba5f931df215e29add054
Raghu Betina <rbetina@gmail.com>
2016-04-15 13:14:37 -0500

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rock, Paper, Scissors -- Rails
+
+Move the logic from your solution to `rps_ruby` into
+
+    app/controllers/game_controller.rb
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,6 +7,11 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
+    # Your logic here
+
+    # In the end, make sure you assign the correct values to the
+    #   following two variables:
+
     @computer_move = "Replace this string with the correct value."
 
     @result = "Replace this string with the correct value."
